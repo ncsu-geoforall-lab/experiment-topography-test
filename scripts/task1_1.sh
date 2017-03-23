2cec8c4e6bdeb3c297b276527e6e6a4bae7af8da
Anna Petrasova <kratochanna@gmail.com>
2017-03-23 00:09:43 -0400

    finish first task

--- scripts/task1_1.sh
+++ scripts/task1_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#export GRASS_FONT="Lato-Medium"
+export GRASS_FONT="Lato-Medium"
 export GRASS_OVERWRITE=1
 
 ME=`basename -s .sh "$0"`
@@ -14,20 +14,34 @@ fi
 g.region n=221035 s=219876 w=323375 e=324801 res=3
 r.neighbors input=ned output=ned_tmp size=5
 r.colors map=ned_tmp rules=../elevation_color.txt
-r.contour input=ned_tmp output=contours_tmp step=20 -t
+r.contour input=ned_tmp output=contours_tmp step=30 -t
 
 d.mon cairo width=800 hei=650 res=1  out=../figures/${ME}.png --o
 d.rast ned_tmp
 d.vect contours_tmp
+X=323770
+Y=220736
+OFFX=20
+OFFY=-20
+let "XX = X + OFFX"
+let "YY = Y + OFFY"
 d.graph -m << EOF
   color black
-  symbol basic/point 2 323770 220706 none black
+  symbol basic/point 2 $X $Y none black
+  move $XX $YY
+  text A
 EOF
-d.text -g text=A at=323770,220706
+X=323943
+Y=220328
+let "XX = X + OFFX"
+let "YY = Y + OFFY"
 d.graph -m << EOF
   color black
-  symbol basic/point 2 323943 220348 none black
+  symbol basic/point 2 $X $Y none black
+  move $XX $YY
+  text B
 EOF
-d.text -g text=A at=323943,220348
+
 d.mon stop=cairo
+
 
