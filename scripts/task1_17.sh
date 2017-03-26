4d9a7f58e8da69335f115166025d7c77afce4ca9
Anna Petrasova <kratochanna@gmail.com>
2017-03-26 17:37:40 -0400

    add task 17

---
+++ scripts/task1_17.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+export GRASS_FONT="Lato-Medium"
+export GRASS_OVERWRITE=1
+
+ME=`basename -s .sh "$0"`
+cd `dirname $0`
+if [ ! -d "../figures" ]; then
+  mkdir ../figures
+fi
+
+
+g.region res=7 n=1150842 s=1147937 w=-2850050 e=-2846480
+r.neighbors input=yosemite output=ned_tmp size=3
+r.colors map=ned_tmp rules=../elevation_color.txt
+r.contour input=ned_tmp output=contours_tmp step=20 cut=10
+
+d.mon cairo width=800 hei=650 res=1  out=../figures/${ME}.png --o
+#d.rast ned_tmp
+d.vect contours_tmp color=139:105:20
+d.vect contours_tmp where="level % 100 = 0" width=3 color=139:105:20
+
+# points
+X=-2847673
+Y=1148294
+d.graph -m << EOF
+  color black
+  width 7
+  symbol basic/box 8 $X $Y black none
+EOF
+X=-2848914
+Y=1150347
+d.graph -m << EOF
+  color black
+  width 7
+  symbol basic/circle 6 $X $Y aqua none
+  text B
+EOF
+
+# contour labels
+d.graph -m << EOF
+  color white
+  polygon
+-2848766.72629 1148548.57071  
+-2848600.41792 1148791.76521
+-2848659.59293 1148840.868
+-2848820.29297 1148584.28183
+EOF
+d.graph -m << EOF
+  color 139:105:20
+  size 2.7
+  move -2848765.65407 1148540.89016 
+  rotation 56
+  text 3400
+EOF
+
+d.graph -m << EOF
+  color white
+  polygon
+-2849170.72362 1148280.91232 
+-2848914.03466 1148133.42894
+-2848860.46798 1148195.9234 
+-2849161.86806 1148372.33456       
+EOF
+d.graph -m << EOF
+  color 139:105:20
+  size 2.7
+  move -2849184.0125 1148290.76788   
+  rotation 330
+  text 3300
+EOF
+
+d.graph -m << EOF
+  color white
+  polygon
+-2847583.7954 1150320.73504
+-2847561.4759 1149963.62384  
+-2847467.7343 1149932.37661  
+-2847472.1981 1150311.80726
+EOF
+d.graph -m << EOF
+  color 139:105:20
+  size 2.7
+  move -2847551.30098 1150289.48781
+  rotation 270
+  text 3400
+EOF
+
+d.mon stop=cairo
+
+echo "\mysection{Task ${ME: -2}}"
+echo "\myimage{${ME}.png}"
+echo "You are standing at the square, but you want to get to a place (on the map) where
+you would be able to see a small lake at the circle. Assume there is no vegetation.
+Please draw a line from the square to another place on the map that indicates
+the route you would take to a spot where you can see the circle. Explain below, why you chose
+the spot as well the route to get there.
+"
---
+++ scripts/task2_17.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+export GRASS_FONT="Lato-Medium"
+export GRASS_OVERWRITE=1
+
+ME=`basename -s .sh "$0"`
+cd `dirname $0`
+if [ ! -d "../figures" ]; then
+  mkdir ../figures
+fi
+
+
+g.region res=7 n=1148518 s=1145613 w=-2862349 e=-2858779
+r.neighbors input=yosemite output=ned_tmp size=3
+r.colors map=ned_tmp rules=../elevation_color.txt
+r.contour input=ned_tmp output=contours_tmp step=20 cut=10
+
+d.mon cairo width=800 hei=650 res=1  out=../figures/${ME}.png --o
+#d.rast ned_tmp
+d.vect contours_tmp color=139:105:20
+d.vect contours_tmp where="level % 100 = 0" width=3 color=139:105:20
+
+# points
+X=-2861533
+Y=1147222
+d.graph -m << EOF
+  color black
+  width 7
+  symbol basic/box 8 $X $Y black none
+EOF
+X=-2859471
+Y=1147133
+d.graph -m << EOF
+  color black
+  width 7
+  symbol basic/circle 6 $X $Y aqua none
+  text B
+EOF
+
+# contour labels
+d.graph -m << EOF
+  color white
+  polygon
+-2860886.18764 1145673.82555 
+-2860756.73483 1145981.83396  
+-2860846.01263 1146039.86453
+-2860979.92933 1145682.75333  
+EOF
+d.graph -m << EOF
+  color 139:105:20
+  size 2.7
+  move -2860890.89876 1145682.75333  
+  rotation 68
+  text 3200
+EOF
+
+d.graph -m << EOF
+  color white
+  polygon
+  -2859493.45396 1146057.72009 
+  -2859444.35117 1145700.60889
+  -2859350.60948 1145709.53667  
+  -2859386.3206 1146062.18398  
+  
+EOF
+d.graph -m << EOF
+  color 139:105:20
+  size 2.7
+  move -2859479.88728 1146035.40064    
+  rotation 276
+  text 3300
+EOF
+
+d.graph -m << EOF
+  color white
+  polygon
+-2859966.6263 1147707.21495
+-2859713.4318 1147831.13165 
+-2859730.2151 1147928.09
+-2860038.0485 1147762.92607
+EOF
+d.graph -m << EOF
+  color 139:105:20
+  size 2.7
+  move -2859977.87353 1147701.67884
+  rotation 30
+  text 3200
+EOF
+
+d.mon stop=cairo
+
+echo "\mysection{Task ${ME: -2}}"
+echo "\myimage{${ME}.png}"
+echo "You are standing at the square, but you want to get to a place (on the map) where
+you would be able to see a small lake at the circle. Assume there is no vegetation.
+Please draw a line from the square to another place on the map that indicates
+the route you would take to a spot where you can see the circle. Explain below, why you chose
+the spot as well the route to get there.
+"
